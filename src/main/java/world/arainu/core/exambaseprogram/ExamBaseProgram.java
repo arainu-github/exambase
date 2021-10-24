f2f9d01dcd71a0cfe8efa6e8f9d006e23669c0c4
Riku Ueda <riku.ueda@outlook.jp>
2021-10-24 13:25:31 +0900

    Initial commit

---
+++ src/main/java/world/arainu/core/exambaseprogram/ExamBaseProgram.java
@@ -0,0 +1,17 @@
+package world.arainu.core.exambaseprogram;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExamBaseProgram extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
